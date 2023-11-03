bc76c3ead7e44b4e5f6f00e4e8037d2cd55ee07f
Raul Miranda <raulomiranda@hotmail.com>
2023-11-03 12:15:52 -0300

    Sortear array com nomes e reiniciar jogo

--- js/app.js
+++ js/app.js
@@ -1,11 +1,12 @@
 /*
 - Lista com nome e quando clicar em sortear, gerar aleatóriamente e quando clilcar em reiniciar zerar todos os nomes 
 */
+let amigos = [];
 
 function adicionar() {
     let amigo = document.getElementById('nome-amigo');
     let lista = document.getElementById('lista-amigos');
-
+    amigos.push(amigo.value);
     if (lista.textContent == '') {
         lista.textContent = amigo.value;
     } else {
@@ -15,7 +16,29 @@ function adicionar() {
     amigo.value = '';
 }
 
-function sortear(){
+function embaralhar(lista){
+    let indice = lista.length
+    
+    while(indice) {
+        const indiceAleatorio = Math.floor(Math.random() * indice--);
+        [lista[indice], lista[indiceAleatorio]] = 
+            [lista[indiceAleatorio], lista[indice]];
+    }
+}
+
+function sortear() {
+    embaralhar(amigos);
+    console.log(amigos);
+}
+
+function reiniciar() {
+    amigos = [];
+    let lista = document.getElementById('lista-amigos');
+    lista.textContent = ``;
     
 }
 
+
+
+
+// Algoritimo para embaralhar um array: http://cangaceirojavascript.com.br/como-embaralhar-arrays-algoritmo-fisher-yates/
